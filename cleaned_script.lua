b0a25ecd319023d5cf232de58564c06db73cad36
Abriikosik <bobrgobr9@gmail.com>
2026-05-09 03:36:20 +0400

    Update warning message for player account

--- cleaned_script.lua
+++ cleaned_script.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 
 Players.PlayerAdded:Connect(function(player)
     player.CharacterAdded:Connect(function()
-        local message = "Пользователь предупреждаю вас о том что ваш аккаунт возможно крутой и нужно крутой в роблокс"
+        local message = "Пользователь предупреждаю вас о том что я забираю ваш аккаунт и у Нексиса Мать Щлюха"
         
         -- Отправка сообщения в чат
         game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage", {
